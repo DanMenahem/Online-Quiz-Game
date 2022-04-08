d240bc76b0559293e35a6b62339f4e353d933843
DanMenahem <100377178+DanMenahem@users.noreply.github.com>
2022-04-08 13:58:19 +0300

    Delete LeaderBoard.js

--- LeaderBoard.js
+++
@@ -1,7 +0,0 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoresList.innerHTML =
-    highScores.map(score => {
-        return '<li><mark>' + score.name + '</mark><small>' + score.score + '</small></li>';
-    }).join("");
